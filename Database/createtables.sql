5f019647edde77b420ca2c3882dd4f2c7fdd55bb
Marco Ferrati <marco.ferrati@gmail.com>
2019-10-17 16:29:45 +0200

    Updated create.sql after revision

--- Database/createtables.sql
+++ Database/createtables.sql
@@ -1,12 +1,10 @@
-
-
 create table amministratori (
     id_amministratore int primary  key auto_increment,
     user_name varchar(32) not null,
     password varchar(48) not null,
     mail varchar (48) not null
 );
-
+ 
 create  table utenti (
     id_utente int primary key auto_increment,
     nome varchar (32) not null ,
@@ -15,38 +13,33 @@ create  table utenti (
     mail varchar (32) not null ,
     password varchar (48) not null ,
     data_nascita date not null ,
-    livello_utenza ENUM ('normale','locatario') default  'normale',
-    nazionalita varchar(32) default "Italia",
-    img_profilo varchar(48) not null,
--- come default dell immagine possiamo prendere tipo il default di facebook
---     caricare la foto, rinominandolo con hash della sua mail
+    img_profilo varchar(48) default “user_image.png”,
+-- aggiornarlo quando avremo accesso ai server
     telefono varchar(18) not null
 );
-
+ 
 create  table  annunci (
     id_annuncio int primary key auto_increment,
     titolo varchar(32) not null ,
     descrizione varchar(512) not null ,
-    img_anteprima varchar(48) default "", -- anche qua come default prendiamo un'immagine di default e lo hashiamo
+    img_anteprima varchar(48) default "house_image.png", -- aggiornarlo quando avremo accesso al server
     indirizzo varchar (128) not null,
-    citta varchar(2) not null,
-    cap varchar(5) not null ,
+    citta varchar(128) not null,
     proprietario int foreign key not null,
-    approvazione enum('si','no'),
+    approvazione tinyint default 0,
     max_ospiti int default 1,
-    prezzo_base float not null ,
-    prezzo_persona float not null,
+    prezzo_notte float not null ,
     foreign  key (proprietario) references utenti(id_utente)
 );
-
+ 
 create  table foto_annunci (
     id_foto int primary key auto_increment,
     file_path varchar(128) not null,
-    descrizione varchar(244) ,
+    descrizione varchar(128)  not null,
     annuncio int not null,
     foreign key (annuncio) references annunci(id_annuncio)
 );
-
+ 
 create table preferiti (
     id_preferito int primary key auto_increment,
     annuncio int not null ,
@@ -54,30 +47,22 @@ create table preferiti (
     foreign key(annuncio) references annunci(id_annuncio),
     foreign key(utente) references utenti(id_utente)
 );
-
+ 
 create table commenti (
     id_prenotazione int primary  key auto_increment,
     data_pubblicazione timestamp DEFAULT CURRENT_TIMESTAMP ,
     titolo varchar(64) not null,
     commento varchar (512) not null,
-    likes int default 0,
-    dislike int default 0,
     votazione enum('1','2','3','4','5')
 );
-
-create  table segnalazioni (
-    id_prenotazione int primary key not null,
-    titolo varchar (32) not null ,
-    motivazione varchar(512) not null,
-    data timestamp  default  CURRENT_TIMESTAMP
-);
+ 
 create table indisponibilita (
     id_indisponibilita int primary  key auto_increment,
     annuncio int not null,
     data_inizio date not null,
     data_fine date not null
 );
-
+ 
 create table prenotazioni (
     id_prenotazione int primary key auto_increment,
     prenotante int not null,
