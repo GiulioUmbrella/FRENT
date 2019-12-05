a5a0e96d4864940c48ed295edb4e9858405596da
Xiaowei <xiaowei.wen1997@gmail.com>
2019-12-05 14:38:59 +0100

    #149 creata la classe Foto

--- php/Foto.php
+++ php/Foto.php
@@ -3,5 +3,53 @@
 
 class Foto
 {
+    private $id_foto;
+    private $file_path;
+    private $descrizione;
+
+    public function __construct($id, $des = "Questa foto non &egrave; stato commentato", $path = "deada")
+    {
+        $this->setIdFoto($id);
+        $this->setFilePath($path);
+        $this->setDescrizione($des);
+
+    }
+
+    public function getIdFoto(): int
+    {
+        return $this->id_foto;
+    }
+
+    public function setIdFoto($id_foto): void{
+        if (is_string($id_foto)) {
+            $this->id_foto = $id_foto;
+        } else {
+//          gestire l'eccezione
+        }
+    }
+
+    public function getFilePath(): string
+    {
+        return $this->file_path;
+    }
+
+    public function setFilePath($file_path): void
+    {
+        if(is_string($file_path)){
+            $this->file_path = $file_path;
+        }else{
+            //gestire l'eccezione
+        }
+    }
+
+    public function getDescrizione(): string
+    {
+        return $this->descrizione;
+    }
+
+    public function setDescrizione($descrizione): void
+    {
+        $this->descrizione = $descrizione;
+    }
 
 }
