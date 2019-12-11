e70d499e3b0c0ca504c1fc8a8b495d1dfe88213e
Xiaowei <xiaowei.wen1997@gmail.com>
2019-12-11 17:50:31 +0100

    Auto stash before checking out "HEAD"

--- php/pagine_php/approvazione_annunci.php
+++ php/pagine_php/approvazione_annunci.php
@@ -6,31 +6,25 @@ require_once "../classi/Database.php";
 $connessione = new Database("localhost", "root", "", "frentdb");
 try {
     session_start();
-    $manager = new Frent($connessione,$_SESSION["admin_obj"]);
-    echo "p1";
+    $manager = new Frent($connessione, $_SESSION["admin_obj"]);
     $annunci = $manager->adminGetAnnunci();
-    echo "p2";
     
     $pagina = file_get_contents("../../html/approvazione_annunci.html");
-    $content = "<div id=\"content\" class=\"list_annunci_pendenti\">
-    <h2>Ci sono " . count($annunci) . " annunci da controllare: </h2> <ul>";
-    echo "p3";
+    $content = "<div id=\"content\" class=\"list_annunci_pendenti\"><h2>Ci sono " . count($annunci) . " annunci da controllare: </h2> <ul>";
     
     foreach ($annunci as $annuncio) {
         $id = $annuncio->getIdAnnuncio();
-        $content .= "<li><a href=";
-        $content .= "\"dettagli_annuncio.html?id=$id\">ID Titolo annuncio</a> <a href=\"dettagli_annunci.php?idAnnuncio=$id&approvato=true\"";
-        $content .= "class=\"link_gestione link_approva\">Approva</a> <a href='?idAnnuncio=$id&approvato=true' ";
-        
-        $content .= 'class=\"link_gestione link_rigetta\">Non approva</a></li>';
+        $content .= "<li>";
+        $content .= "<a href=\"dettagli_annuncio.php?id=$id\">ID Titolo annuncio</a>";
+        // todo
+        $content .= "<a href=\"approvazione_annuncio.php?idAnnuncio=$id&approvato=true\" class=\"link_gestione link_approva\">Approva</a>";
+        $content .= "<a href=\"?idAnnuncio=$id&approvato=true\" class=\"link_gestione link_rigetta\">Non approva</a></li>";
     }
-    
     $content .= "  </ul></div>";
     
     $pagina = str_replace("<Flag1/>", $content, $pagina);
     echo $pagina;
     
 } catch (Eccezione $ex) {
-  
-    echo "eccezione". $ex->getMessage();
+    echo "eccezione" . $ex->getMessage();
 }
