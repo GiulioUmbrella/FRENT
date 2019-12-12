b52af04846cca24b6ac7bdcc5f17d13f4fd18211
TommasoAzz <azzalintommaso@gmail.com>
2019-12-12 14:26:14 +0100

    #183 ricercaAnnunci controllo parametri ok

--- php/classi/Frent.php
+++ php/classi/Frent.php
@@ -38,7 +38,9 @@ class Frent {
     public function ricercaAnnunci($citta, $numOspiti, $dataInizio, $dataFine): array {
         try {
             $this->db_instance->connect();
-            // if(!is_int($numOspiti) && ) throw new Eccezione("Formato parametro errato");
+            if(!is_int($numOspiti) || !checkIsValidDate($dataInizio) || !checkIsValidDate($dataFine) || !checkDateBeginAndEnd($dataInizio, $dataFine)) {
+                throw new Eccezione("Parametri di invocazione di ricercaAnnunci errati.");
+            }
             $procedure_name_and_params = "ricerca_annunci(\"$citta\", $numOspiti, \"$dataInizio\", \"$dataFine\")";
             $lista_annunci = $this->db_instance->queryProcedure($procedure_name_and_params);
         } catch(Eccezione $exc) {
