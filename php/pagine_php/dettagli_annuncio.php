917cb452736e15d595a76b25eda5d5cb4f8739e8
Xiaowei <xiaowei.wen1997@gmail.com>
2019-12-12 16:14:36 +0100

    WIP dettagli_annunci.php

--- php/pagine_php/dettagli_annuncio.php
+++ php/pagine_php/dettagli_annuncio.php
@@ -10,24 +10,40 @@ try {
     $commenti = $manager->getCommentiAnnuncio($id);
     $foto = $manager->getFotoAnnuncio($id);
     $pagina = file_get_contents("../../html/dettagli_annunci.html");
-    $pagina = str_replace("<TITOLO_ANNUNCIO/>",$annuncio->getTitolo(),$pagina);
+    $pagina = str_replace("<TITOLO_ANNUNCIO/>", $annuncio->getTitolo(), $pagina);
     //todo calcolare la media dei commenti
-    $totale=0;
-    $str_commenti="";
-    foreach ($commenti as $commento){
-        $totale +=intval( $commenti->getVotazione());
+    $totale = 0;
+    $str_foto = "";
+    foreach ($foto as $f) {
         
-        $str_commenti.="";
+        
+        $str_commenti .= "";
         
     }
     //
-    $mediaCommenti=0;
-    $pagina = str_replace("<Valutazione/>",$mediaCommenti,$pagina);
+    $mediaCommenti = 0;
     
     
+    $str_commenti .= "<div id=\"commenti\"><h2>Commenti</h2><ul>";
+    foreach ($commenti as $commento) {
+        $totale += intval($commenti->getVotazione());
+        $immagine_profilo = "";
+        $testo_commento = $commento->getCommento();
+        $votazione = $commento->getVotazione();
+        $user_name = "";
+        $data_commento = $commento->getDataPubblicazione();
+        $titolo_commento = $commento->getTitolo();
+        $str_commenti .= "<li><div class=\"intestazione_commento\"><img src=\"$immagine_profilo\" alt=\"\"/>
+                <div><p class=\"username_commento\">$user_name</p><p class=\"data_commento\">$data_commento</p>
+                </div><p class=\"totale_commenti_utente\">Numero Commenti Totali</p>
+                </div><div class=\"corpo_commento\"><h1>$titolo_commento</h1><p>Votazione: $votazione</p>
+                <p>$testo_commento</p></div></li>";
+    }
     
-    $_SESSION["manager"] = $manager;
-    $pagina = str_replace("<Flag1/>", $content, $pagina);
+    
+    $pagina .= "</ul></div>";
+    $pagina = str_replace("<Valutazione/>", $mediaCommenti, $pagina);
+    $pagina = str_replace("<Commenti/>", $str_commenti, $pagina);
     echo $pagina;
     
 } catch (Eccezione $ex) {
