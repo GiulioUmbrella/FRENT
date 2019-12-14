1cbbf49692bdc80219b91d62395959034915a624
Xiaowei <xiaowei.wen1997@gmail.com>
2019-12-14 14:15:08 +0100

    minor fixing

--- php/pagine_php/registrazione.php
+++ php/pagine_php/registrazione.php
@@ -1,5 +1,9 @@
 <?php
 require_once "../classi/Frent.php";
+session_start();
+if (isset($_SESSION["user"])){
+    header("Location: index.php");
+}
 $pagina = file_get_contents("../components/registrazione.html");
 
 $pagina = str_replace("<HEADER/>",file_get_contents("../components/header_no_logged.html"),$pagina);
