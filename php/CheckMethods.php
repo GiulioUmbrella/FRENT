fc01d970f0d3ef9a87ffe4709acc0e86a142cb8d
TommasoAzz <azzalintommaso@gmail.com>
2019-12-16 23:53:23 +0100

    ottimizzato checkmethods

--- php/CheckMethods.php
+++ php/CheckMethods.php
@@ -1,79 +1,58 @@
 <?php
 
 /**
- * Class Amministratore
- * 1.
- * 2.
- * 3.
- * 4. metodo per controllare il range dei interi
- *      id delle entita
- *      numMaxOspiti di un annuncio
- *
- * 5.
- * 6.
- * 7. controllare che la durata sia maggiore
- * 8.
- *
- */
-//metodo per verificare la correttezza delle mail e della sua lunghezza done
-/**
- * Controlla se la $mail è una mail valida ovvero ha qualcosa@qualunquecosa.qualunque cosa e
+ * Controlla se l'indirizzo e-mail passato come parametro è valido, ovvero è nella forma A@B.C, con A, B, C stringhe e 
  * che sia di lunghezza inferiore a $lunghezza
- * @param $mail string mail da controllare
- * @param $lunghezza int lunghezza massima che la mail può assumere
+ * @param string $mail mail da controllare
+ * @param int $lunghezza lunghezza massima che la mail può assumere
  * @return bool sse $mail è un indirizzo email valida e strlen(mail) <= lunghezza
  */
 function checkIsValidMail($mail, $lunghezza=191): bool{
-    return is_string($mail) and filter_var(trim($mail), FILTER_VALIDATE_EMAIL) and strlen(trim($mail)) < $lunghezza;
+    return is_string($mail) and filter_var(trim($mail), FILTER_VALIDATE_EMAIL) and checkStringMaxLen($mail, $lunghezza);
 }
-
 
 /**
  * Controlla se in una stringa è contenuto il carattere SPAZIO
- * @param $str string la stringa da controllare
+ * @param string $str la stringa da controllare
  * @return bool restituisce true sse la stringa non contiene nessun spazio
  */
 function checkStringContainsNoSpace($str): bool {
     return is_string($str) and !preg_match("/^\S{6,}\z/",trim($str));
 }
 
-
 /**
  * Controlla se il valore passato è una stringa e se è di lunghezza <= a $len
- * @param $str string stringa da controllare
- * @param $len int indica la lunghezza massima che la stringa può avere.
+ * @param string $str stringa da controllare
+ * @param int $len indica la lunghezza massima che la stringa può avere.
  * @return bool se str è stringa e len è un intero e la stringa ha la lunghezza <= a int, altrimenti false;
  */
-function checkStringMaxLen($str, $len): bool{
+function checkStringMaxLen($str, $len): bool {
     return is_string($str) and is_int($len) and strlen($str) <= $len;
 }
+
 /**
  * Controlla se il valore passato è una stringa e se è di lunghezza >= a $len
- * @param $str string stringa da controllare
- * @param $len int indica la lunghezza massima che la stringa può avere.
+ * @param string $str stringa da controllare
+ * @param int $len indica la lunghezza massima che la stringa può avere.
  * @return bool se str è stringa e len è un intero e la stringa ha la lunghezza <= a int, altrimenti false;
  */
 function checkStringMinLen($str, $len): bool{
     return is_string($str) and is_int($len) and strlen($str) >= $len;
 }
 
-
-
 /**
  * controlla se il valore $date contiene una data valida secondo il formato specificato.
- * @param $date string la stringa che dovrebbe contenere la data.
+ * @param string $date la stringa che dovrebbe contenere la data.
  * @param string $format il formato in cui è rappresentato la data.
- * @return bool restituisce true sse $data è una data valida rispetto al formato specificato.
+ * @return bool restituisce true sse $date è una data valida rispetto al formato specificato.
  */
 function checkIsValidDate($date, $format="Y-m-d"){
     return is_string($date) and date("Y-m-d",strtotime($date));
-
 }
 
-
 /**
- * La funzione controlla che in una stringa non siano presenti i caratteri numerici
- * @param $str string è la stringa da controllare
+ * La funzione controlla che in una stringa non siano presenti caratteri numerici.
+ * @param string $str è la stringa da controllare
  * @return bool restituisce true sse la stringa non contiene nessun carattere numerico.
  */
 function checkStringNoNumber($str):bool {
@@ -81,19 +60,19 @@ function checkStringNoNumber($str):bool {
 }
 
 /**
- * @param $dataI
- * @param $dataF
+ * Verifica che $dataI sia una data precedente a $dataF (che avviene prima).
+ * @param string $dataI prima string contenente una data da verificare che sia inferiore a $dataF
+ * @param string $dataF seconda string contenente una data da verificare che sia superiore a $dataF
  * @return bool restuisce true sse la dataI è antecedente alla dataF
  */
 function checkDateBeginAndEnd($dataI, $dataF): bool {
-    return is_string($dataI) and is_string($dataF) and checkIsValidDate($dataI)
-        and checkIsValidDate($dataF) and strtotime($dataI)<= strtotime($dataF);
+    return checkIsValidDate($dataI) and checkIsValidDate($dataF) and strtotime($dataI) <= strtotime($dataF);
 }
-//verificare il numero di telefono
+
 /**
- * verifica se la stringa $telefono contiene un numero di telefono valido.
- * @param $telefono string numero da controllare.
- * @return bool
+ * Verifica che la stringa $telefono contenga un numero di telefono valido.
+ * @param string $telefono stringa contenente (possibilmente) un numero di telefono
+ * @return bool true sse $telefono rappresenta un numero di telefono
  */
 function checkPhoneNumber($telefono): bool {
     return true;
