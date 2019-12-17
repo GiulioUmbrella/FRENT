97042dddb7e89e6b03cb032ef5c55ccf271783f0
Xiaowei <xiaowei.wen1997@gmail.com>
2019-12-17 12:02:19 +0100

    Refactored login admin

--- php/pagine_php/login_admin.php
+++ php/pagine_php/login_admin.php
@@ -4,34 +4,35 @@ require_once "../classi/Frent.php";
 
 require_once "../CredenzialiDB.php";
 $pagina = file_get_contents("../components/login_admin.html");
+$pagina = str_replace("<FORM/>", file_get_contents("../components/login_form.html"), $pagina);
+$pagina = str_replace("<PAGE/>", "./login_admin.php", $pagina);
 session_start();
-//if(isset($_GET["error_code"])){
-//
-//
-//}
-//
-//$pagina = str_replace("<FOOTER/>", file_get_contents("../components/footer.html"), $pagina);
-
-if (isset($_POST["accedi"])){
+if (isset($_POST["accedi"])) {
     
+    $nome = $_POST["user"];
+    $password = $_POST["password"];
     try {
         $db = new Database(CredenzialiDB::DB_ADDRESS, CredenzialiDB::DB_USER,
             CredenzialiDB::DB_PASSWORD, CredenzialiDB::DB_NAME);
     
-        $nome = $_POST["user"];
-        $password= $_POST["password"];
         $frent = new Frent($db);
         $admin = $frent->adminLogin($nome, $password);
     
-        session_start();
         $_SESSION["admin"] = $admin;
         header("Location: ../pagine_php/approvazione_annunci.php");
-    
+        
     } catch (Eccezione $e) {
-        $pagina = str_replace("<p id=\"credenziali_errate\"></p>",
-        "<p id=\"credenziali_errate\">Credenziali errate!</p>", $pagina);
+        
+        $pagina = str_replace("<div id=\"credenziali_errate\"></div>",
+            "<div id=\"credenziali_errate\"><p>Credenziali errate!</p></div>", $pagina);
+        $pagina = str_replace("<VALUEUSERNAME>", "value=\"$nome\"", $pagina);
+        $pagina = str_replace("<VALUEPASSWORD>", "value=\"$password\"", $pagina);
     }
     
+} else {
+    $pagina = str_replace("<VALUEUSERNAME>", " ", $pagina);
+    $pagina = str_replace("<VALUEPASSWORD>", " ", $pagina);
 }
+$pagina= str_replace("<FOOTER/>",file_get_contents("../components/footer.html"),$pagina);
 
 echo $pagina;
