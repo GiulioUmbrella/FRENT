079c7efb0d51b8a5c63f172c7476f3979d0b310c
Xiaowei <xiaowei.wen1997@gmail.com>
2019-12-17 14:56:58 +0100

    WIP dettaglio_annuncio

--- php/pagine_php/dettagli_annuncio.php
+++ php/pagine_php/dettagli_annuncio.php
@@ -20,9 +20,9 @@ try {
         $pagina = str_replace("<HEADER/>",file_get_contents("../components/header_no_logged.html"),$pagina);
     
     }
-//    $pagina = str_replace("<TITOLO_ANNUNCIO/>", $annuncio->getTitolo(), $pagina);
     $pagina = str_replace("<TITOLO_ANNUNCIO/>", $annuncio->getTitolo(), $pagina);
-    //todo calcolare la media dei commenti
+//    $pagina = str_replace("<TITOLO_ANNUNCIO/>", $annuncio->getTitolo(), $pagina);
+
 //    $totale = 0;
 //    $str_foto = "";
 //    foreach ($foto as $f) {
@@ -34,33 +34,33 @@ try {
 //    //
     
     $str_commenti = "";
-//    try {
-//        $mediaCommenti = 0;
-//        $commenti = $manager->getCommentiAnnuncio($id);
-//        $commenti = array();
-//        $str_commenti .= "<ul>";
-//        foreach ($commenti as $commento) {
-//            $totale += intval($commenti->getVotazione());
-//            $immagine_profilo = "";
-//            $testo_commento = $commento->getCommento();
-//            $votazione = $commento->getVotazione();
-//            $user_name = "";
-//            $data_commento = $commento->getDataPubblicazione();
-//            $titolo_commento = $commento->getTitolo();
-//            $str_commenti .= "<li><div class=\"intestazione_commento\"><img src=\"$immagine_profilo\" alt=\"\"/>
-//                <div><p class=\"username_commento\">$user_name</p><p class=\"data_commento\">$data_commento</p>
-//                </div><p class=\"totale_commenti_utente\">Numero Commenti Totali</p>
-//                </div><div class=\"corpo_commento\"><h1>$titolo_commento</h1><p>Votazione: $votazione</p>
-//                <p>$testo_commento</p></div></li>";
-//        }
-//
-//        $str_commenti .= "</ul>";
-//        $pagina = str_replace("<Commenti/>", $str_commenti, $pagina);
-//        $pagina = str_replace("<Valutazione/>", $mediaCommenti, $pagina);
-//    }catch(Eccezione $e){
-//        $pagina = str_replace("<Commenti/>","<p>Ancora non ci sono commenti!</p>", $pagina);
-//
-//    }
+    try {
+        $mediaCommenti = 0;
+        $commenti = $manager->getCommentiAnnuncio($id);
+        $commenti = array();
+        $str_commenti .= "<ul>";
+        foreach ($commenti as $commento) {
+            $totale += intval($commenti->getVotazione());
+            $immagine_profilo = "";
+            $testo_commento = $commento->getCommento();
+            $votazione = $commento->getVotazione();
+            $user_name = "";
+            $data_commento = $commento->getDataPubblicazione();
+            $titolo_commento = $commento->getTitolo();
+            $str_commenti .= "<li><div class=\"intestazione_commento\"><img src=\"$immagine_profilo\" alt=\"\"/>
+                <div><p class=\"username_commento\">$user_name</p><p class=\"data_commento\">$data_commento</p>
+                </div><p class=\"totale_commenti_utente\">Numero Commenti Totali</p>
+                </div><div class=\"corpo_commento\"><h1>$titolo_commento</h1><p>Votazione: $votazione</p>
+                <p>$testo_commento</p></div></li>";
+        }
+
+        $str_commenti .= "</ul>";
+        $pagina = str_replace("<Commenti/>", $str_commenti, $pagina);
+        $pagina = str_replace("<Valutazione/>", $mediaCommenti, $pagina);
+    }catch(Eccezione $e){
+        $pagina = str_replace("<Commenti/>","<p>Ancora non ci sono commenti!</p>", $pagina);
+
+    }
     echo $pagina;
     
 } catch (Eccezione $ex) {
