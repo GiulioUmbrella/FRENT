4ae4790fe22cef8a2cb66e7ad5fe515eec72c712
TommasoAzz <azzalintommaso@gmail.com>
2019-12-17 13:29:21 +0100

    #198 fix procedura sql documentazione

--- Database/Operazioni/get_ultimi_annunci_approvati.sql
+++ Database/Operazioni/get_ultimi_annunci_approvati.sql
@@ -1,14 +1,13 @@
-/*Procedura per ottenere i dettagli di un annuncio
-PRE: id è l'identificativo di un annuncio
-Restituisce il reocrd relativo all'ID passato come parametro
+/* Procedura per ottenere i dettagli di un annuncio
+Restituisce i record degli ultimi sei annunci approvati dagli amministratori
 */
 DROP PROCEDURE IF EXISTS get_ultimi_annunci_approvati;
 DELIMITER |
 CREATE PROCEDURE get_ultimi_annunci_approvati()
 BEGIN
-    SELECT id_annuncio,titolo, img_anteprima
+    SELECT id_annuncio, titolo, img_anteprima
     FROM annunci
-    where stato_approvazione=1
+    WHERE stato_approvazione=1
     ORDER BY id_annuncio DESC
     LIMIT 6;
 END |
