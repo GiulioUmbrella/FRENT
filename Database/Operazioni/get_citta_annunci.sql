722fda9cb4659c51c3035769af3b55afce3b3d77
TommasoAzz <azzalintommaso@gmail.com>
2019-12-21 00:13:46 +0100

    #221 preparata procedura SQL

---
+++ Database/Operazioni/get_citta_annunci.sql
@@ -0,0 +1,12 @@
+/*Procedura per ottenere tutte le città in cui è attivo Frent (ovvero dove ci sono annunci)
+Resituisce tutte le città in cui ci sono annunci
+*/
+DROP PROCEDURE IF EXISTS get_citta_annunci;
+DELIMITER |
+CREATE PROCEDURE get_citta_annunci()
+BEGIN
+    SELECT DISTINCT citta
+    FROM annunci
+    WHERE bloccato = 0 AND stato_approvazione = 1;
+END |
+DELIMITER ;
