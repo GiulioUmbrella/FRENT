824f4bea017806717ae9ba3483efd0587faf0139
TommasoAzz <azzalintommaso@gmail.com>
2019-12-20 23:14:24 +0100

    aggiornato index di test per supportare caricamenti di file multipli, upload di test con print di log, aggiornati metodi classe

--- php/class_ImageManager.php
+++ php/class_ImageManager.php
@@ -2,6 +2,7 @@
 
 require_once("Eccezione.php");
 define("MB", 1048576);
+
 /**
  * Class ImageManager
  * Classe che si occupa di gestire il caricamento delle immagini sul server,
@@ -9,8 +10,9 @@ define("MB", 1048576);
  */
 class ImageManager {
     private $targetFolder;
-    private $targetFile;
-    private $tempFile;
+    private $targetFileName;
+    private $targetFileExtension;
+    private $tempFileName;
     private $maxFileSize;
 
     /**
@@ -19,39 +21,63 @@ class ImageManager {
      * @param int $maxFileSize dimensione massima di un file caricato
      */
     public function ImageManager($targetFolder, $maxFileSize = 2) {
+        // valori costanti una volta inizializzati
         $this->targetFolder = $targetFolder;
-        $this->targetFile = "";
         $this->maxFileSize = $maxFileSize;
+
+        $this->targetFileName = "";
+        $this->targetFileExtension = "";
+        $this->tempFileName = "";
     }
 
     /**
      * Recupera il file da caricare sul server da $_SERVER. Il file deve avere una delle seguenti estensioni: jpg, png, jpeg.
      * @param string $img_name_attr valore dell'attributo name dell'elemento <input type="file" ..></file>
      * nel form da cui si sta recuperando l'immagine
-     * @param string $fileName nome da assegnare al file una volta caricato (se non assegnato viene preso il nome del file caricato)
+     * @param int $fileIndex indice del file nell'array $_FILES
+     * @param string $fileName nome da assegnare al file una volta caricato, senza estensione (se non assegnato viene preso il nome del file caricato)
      * @throws Eccezione se il file caricato non è un'immagine, se il file supera la dimensione massima specificata nella costruzione e se l'estensione non è valida.
      */
-    public function setFile($img_name_attr, $fileName = "") {
-        $file = $_FILES[$img_name_attr];
-        $this->tempFile = $file["tmp_name"];
-        $extension = pathinfo($file["name"], PATHINFO_EXTENSION);
+    public function setFile($img_name_attr, $fileIndex = 0, $outFileName = "") {
+        // mi assicuro che i campi dati del file siano azzerati
+        $this->unsetFile();
 
+        // verifico che l'indice del file chiesto sia nei limiti
+        if($fileIndex > $this->countFiles($img_name_attr)) {
+            throw new Eccezione("È stato richiesto di accedere ad un file non caricato.");
+        }
+
+        // recupero il nome del file caricato
+        $name = $_FILES[$img_name_attr]["name"][$fileIndex];
+        
+        // file size
+        $size = $_FILES[$img_name_attr]["size"][$fileIndex];
+        
+        // recupero l'estensione del file
+        $this->targetFileExtension = strtolower(pathinfo($name, PATHINFO_EXTENSION));
+        
+        // verifico se è stato richiesto un nome particolare per il file di output
+        $this->targetFileName = $this->targetFolder . ($outFileName === "" ? $name : $outFileName . "." . $this->targetFileExtension);
+        
+        // directory in cui è salvato il file temporaneo
+        $this->tempFile = $_FILES[$img_name_attr]["tmp_name"][$fileIndex];
+        
+        // verifico che l'estensione sia corretta
+        if ($this->targetFileExtension != "jpg" &&
+            $this->targetFileExtension != "png" &&
+            $this->targetFileExtension != "jpeg"
+        ) {
+            throw new Eccezione("Il file caricato non ha un'estensione valida.");
+        }
+        
+        // verifico che il file caricato sia effetivamente un file imamgine (verifica i metadati)
         if (getimagesize($this->tempFile) === false) {
             throw new Eccezione("Il file caricato non è un'immagine.");
         }
-
-        if ($extension != "jpg" && $extension != "png" && $extension != "jpeg") {
-            throw new Eccezione("Il file caricato non ha un'estensione valida.");
-        }
-
-        if ($file["size"] > $this->maxFileSize * MB) {
-            throw new Eccezione("Il file caricato supera la dimensione massima di " . $this->maxFileSize . "MB.");
-        }
         
-        if($fileName === "") {
-            $this->targetFile = $this->targetFolder . basename($_SERVER[$img_name_attr]["name"]);
-        } else {
-            $this->targetFile = $this->targetFolder . basename("$fileName.$extension");
+        // verifico che il file non superi la dimensione massima consentita
+        if ($size > $this->maxFileSize * MB) {
+            throw new Eccezione("Il file caricato supera la dimensione massima di " . $this->maxFileSize . "MB.");
         }
     }
 
@@ -61,11 +87,9 @@ class ImageManager {
      * @throws Eccezione se il file non è stato ancora impostato
      */
     public function fileName(): string {
-        if (strlen($this->targetFile) === 0) {
-            throw new Eccezione("Non è stato ancora selezionato un file.");
-        }
+        $this->verifyFilePresence();
 
-        return $this->targetFile;
+        return $this->targetFileName;
     }
 
     /**
@@ -74,11 +98,9 @@ class ImageManager {
      * @throws Eccezione se il file non è stato ancora impostato
      */
     public function fileExtension(): string {
-        if (strlen($this->targetFile) === 0) {
-            throw new Eccezione("Non è stato ancora selezionato un file.");
-        }
+        $this->verifyFilePresence();
 
-        return strtolower(pathinfo($this->targetFile, PATHINFO_EXTENSION));
+        return $this->targetFileExtension;
     }
 
     /**
@@ -87,10 +109,36 @@ class ImageManager {
      * @throws Eccezione se il file non è stato ancora impostato
      */
     public function saveFile(): bool {
-        if (strlen($this->targetFile) === 0) {
+        $this->verifyFilePresence();
+
+        return move_uploaded_file($this->tempFileName, $this->targetFileName);
+    }
+
+    /**
+     * Restituisce il numero dei file caricati tramite il form.
+     * @param string $img_name_attr valore dell'attributo name dell'elemento <input type="file" ..></file>
+     * nel form da cui si sta recuperando l'immagine
+     */
+    public function countFiles($img_name_attr): int {
+        return count($_FILES[$img_name_attr]["name"]);
+    }
+
+    /**
+     * Verifica che il file sia stato impostato. Se lo è stato non fa nulla, altrimenti lancia un'eccezione.
+     * @throws Eccezione se il file non è stato ancora impostato
+     */
+    private function verifyFilePresence() {
+        if (strlen($this->targetFileExtension) === 0 || strlen($this->targetFileName) === 0) {
             throw new Eccezione("Non è stato ancora selezionato un file.");
         }
+    }
 
-        return move_uploaded_file($this->tempFile, $this->targetFile);
+    /**
+     * Si assicura che i campi dati siano azzerati prima di impostare un file. Se non lo sono, li azzera.
+     */
+    private function unsetFile() {
+        $this->targetFileName = "";
+        $this->targetFileExtension = "";
+        $this->tempFileName = "";
     }
 }
--- upload.php
+++ upload.php
@@ -3,12 +3,12 @@ require_once("php/class_ImageManager.php");
 
 if(isset($_POST['submit'])) {
     $imageManager = new ImageManager("uploads/");
-    
     try {
-        $imageManager->setFile("fileToUpload", "test");
-        echo "File name: " . $imageManager->fileName() . PHP_EOL . "File extension: " . $imageManager->fileExtension() . PHP_EOL;
-    
-        echo $imageManager->saveFile() ? "File has been saved correctly." : "File has not been saved correctly.";
+        for($i = 0, $numFiles = $imageManager->countFiles("fileToUpload"); $i < $numFiles; $i++) {
+            $imageManager->setFile("fileToUpload", $i, "test$i");
+            echo "File name: " . $imageManager->fileName() . ", " . "file extension: " . $imageManager->fileExtension() . ".<br />";
+            echo $imageManager->saveFile() === false ? "File has not been saved correctly.<br />" : "File has been saved correctly.<br />";
+        }
     } catch(Eccezione $exc) {
         echo $exc->getMessage();
     }
