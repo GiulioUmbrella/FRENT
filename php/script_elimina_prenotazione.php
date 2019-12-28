517657bbfeab81663a2e412188d7a016be05126b
Xiaowei <xiaowei.wen1997@gmail.com>
2019-12-28 21:15:10 +0100

    close #173 script elimina prenotazione

--- php/script_elimina_prenotazione.php
+++ php/script_elimina_prenotazione.php
@@ -1,8 +1,8 @@
 <?php
 require_once "load_Frent.php";
 
-if (isset($_POST["id"])){
-    echo "trovato id =".$_POST["id"];
-    $frent->deleteOccupazione(intval($_POST["id"]));
+if (isset($_GET["id"])){
+    echo "trovato id =".$_GET["id"];
+    $frent->deleteOccupazione(intval($_GET["id"]));
 }
 //header("Location: ./mie_prenotazioni.php");
