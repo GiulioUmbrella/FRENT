8e8e0a9b70a5a3382559c79cc34a1e6fb2aaf27b
Xiaowei <xiaowei.wen1997@gmail.com>
2020-01-07 14:36:45 +0100

    pagina di redirect

--- index.php
+++ index.php
@@ -1,15 +1,3 @@
 `<?php
-
-require_once "php/CheckMethods.php";
-
-
-$dataB = "2019-2-21";
-$dataF = "2019-2-27";
-
-
-
-if (checkDateBeginAndEnd($dataB, $dataF)){
-    echo "B & E ok";
-}else
-    echo "B & E Nok";
+header("Location: ./php/index.php");
     
