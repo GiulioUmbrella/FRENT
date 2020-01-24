02e8df09940b0c715833462b9036a1528dfc0c7a
TommasoAzz <azzalintommaso@gmail.com>
2020-01-24 16:19:40 +0100

    aggiunta la possibilità di commentare prenotazione e visualizzare commento in riepilogo prenotazione #178

--- php/riepilogo_prenotazione.php
+++ php/riepilogo_prenotazione.php
@@ -1,79 +1,177 @@
 <?php
 // funziona e valida.
-$pagina = file_get_contents("./components/riepilogo_prenotazione.html");
 require_once "load_Frent.php";
-if (isset($_SESSION["user"])) {
-    try {
-        $pagina = str_replace("<HEADER/>", file_get_contents("./components/header_logged.html"), $pagina);
-        $pagina = str_replace("<FOOTER/>", file_get_contents("./components/footer.html"), $pagina);
+require_once "./components/form_functions.php";
+
+/**
+ * Funzioni con l'unico scopo di evitare duplicazione di codice.
+ */
+
+/**
+ * Sostituisce i placeholder.
+ * @param array $post corrisponde all'array $_POST, da passare come parametor
+ * @param string $pagina aggiornata per riferimento con i valori dei placeholder presi da $_POST
+ */
+function placeholder_replacement_with_content($post, &$pagina) {
+    // ripristino i dati
+    $pagina = replacePlaceholders(
+        $pagina,
+        ["<VALUETITOLOCOMMENTO/>","<VALUETESTOCOMMENTO/>", "<VALUEVOTOCOMMENTO/>",],
+        [$post["titolo_commento"], $post["testo_commento"], $post["valutazione_commento"]]
+    );
+}
+
+/**
+ * Sostituisce i placeholder con stringhe vuote.
+ * @param string $pagina aggiornata per riferimento con i valori dei placeholder sostituiti da stringhe vuote
+ */
+function placeholder_replacement_with_empty(&$pagina) {
+    // ripristino i dati
+    $pagina = replacePlaceholders(
+        $pagina,
+        ["<VALUETITOLOCOMMENTO/>","<VALUETESTOCOMMENTO/>", "<VALUEVOTOCOMMENTO/>",],
+        ["", "", ""]
+    );
+}
+
+
+$pagina = file_get_contents("./components/riepilogo_prenotazione.html");
+if (!isset($_SESSION["user"])) {
+    header("Location: ./login.php");
+}
+
+$pagina = str_replace("<HEADER/>", file_get_contents("./components/header_logged.html"), $pagina);
+$pagina = str_replace("<FOOTER/>", file_get_contents("./components/footer.html"), $pagina);
 //    todo sistemare il modo per arrivare al riepilogo prenotazione
-        
-        
-        if (isset($_GET["id"])) {
-            $id_prenotazione = intval($_GET["id"]);
-            if ($frent->getOccupazione($id_prenotazione)->getIdUtente() != $_SESSION["user"]->getIdUtente()) {
-                header("Location: ./404.php");
-            }
-            $prenotazioni = $frent->getOccupazione(intval($id_prenotazione));
-            $annuncio = $frent->getAnnuncio($prenotazioni->getIdAnnuncio());
-            $host = $frent->getUser($annuncio->getIdHost());
-            $durata = abs(strtotime($prenotazioni->getDataFine()) - strtotime($prenotazioni->getDataInizio())) / (3600 * 24);
-            $id = $annuncio->getIdAnnuncio();
-            $titolo = $annuncio->getTitolo();
-            $totale = $durata * $annuncio->getPrezzoNotte() * $prenotazioni->getNumOspiti();
-            $pagina = str_replace("<IDPRENOTAZIONE/>", $prenotazioni->getIdOccupazione(), $pagina);
-            $pagina = str_replace("<DATAINIZIO/>", $prenotazioni->getDataInizio(), $pagina);
-            $pagina = str_replace("<DATAFINE/>", $prenotazioni->getDataFine(), $pagina);
-            $pagina = str_replace("<MAILPROPRIETARIO/>", $host->getMail(), $pagina);
-            $pagina = str_replace("<NUMOSPITI/>", $prenotazioni->getNumOspiti(), $pagina);
-            $pagina = str_replace("<NOMEANNUNCIO/>", "<a href=\"./dettagli_annuncio.php?id=$id\" title=\"Visualizza altre informazioni dell\'annuncio $titolo\">$titolo</a>", $pagina);
-            $pagina = str_replace("<INDIRIZZO/>", $annuncio->getIndirizzo(), $pagina);
-            $pagina = str_replace("<CITTA/>", $annuncio->getCitta(), $pagina);
-            $pagina = str_replace("<PROPRIETARIO/>", $host->getUserName(), $pagina);
-            $pagina = str_replace("<PREZZO/>", $totale, $pagina);
 
-            $commentiAnnuncio = $frent->getCommentiAnnuncio($prenotazioni->getIdAnnuncio());
-            // ricerca del commento fatto dall'utente
-            $commentoUtente = Commento::build();
-            $commentoTrovato = false;
-            $i = 0; $l = count($commentiAnnuncio);
-            while($i < $l && !$commentoTrovato) {
-                if($commentiAnnuncio[$i]->getIdPrenotazione() !== $id_prenotazione) {
-                    $i++;
-                } else {
-                    $commentoTrovato = true;
-                    $commentoUtente = $commentiAnnuncio[$i];
-                }
-            }
+/**
+ * $_GET["id"] lo setto se provengo dalla pagina "Le mie prenotazioni"
+ * $_POST["id_prenotazione"] lo setto se ho premuto pubblica commento nel form di pubblicazione del commento
+ */
+if (!isset($_GET["id"]) && !isset($_POST["id_prenotazione"])) {
+    header("Location: 404.php");
+}
 
-            $content = "";
-            if($commentoTrovato) {
-                $content = "<ul class=\"riepilogo_annucio\">";
-                $content .= "<li>Titolo: " . $commentoUtente->getTitolo() . "</li>";
-                $content .= "<li>Valutazione: " . $commentoUtente->getValutazione() . "</li>";
-                $content .= "<li>Commento: " . $commentoUtente->getCommento() . "</li>";
-                $content .= "</ul>";
-            } else {
-		$content = "";
-		if ($prenotazioni->getDataFine() < date("Y-m-d")) {
-		    $content = file_get_contents("./components/aggiungi_commento_form.html");
-		}
-		else {
-		    $content = "<p>Non &egrave; ancora possibile commentare questa prenotazione.</p>";
-		}
-            }
+/**
+ * Quindi ho $_GET["id"] oppure $_POST["id_prenotazione"].
+ * Scopriamo quale dei due.
+ */
+$id_prenotazione = isset($_GET["id"]) ? intval($_GET["id"]) : intval($_POST["id_prenotazione"]);
 
-            $pagina = str_replace("<COMMENTO/>", $content, $pagina);
-            
-            echo $pagina;
+try {
+    $prenotazioni = $frent->getOccupazione($id_prenotazione);
+    $annuncio = $frent->getAnnuncio($prenotazioni->getIdAnnuncio());
+    $host = $frent->getUser($annuncio->getIdHost());
+    
+    if ($prenotazioni->getIdUtente() != $_SESSION["user"]->getIdUtente()) {
+        header("Location: ./404.php");
+    }
+
+    // reperimento dati per visualizzazione
+    $durata = abs(strtotime($prenotazioni->getDataFine()) - strtotime($prenotazioni->getDataInizio())) / (3600 * 24);
+    $totale = $durata * $annuncio->getPrezzoNotte() * $prenotazioni->getNumOspiti();
+    $id = $annuncio->getIdAnnuncio();
+    $titolo = $annuncio->getTitolo();
+    
+    $commentiAnnuncio = $frent->getCommentiAnnuncio($prenotazioni->getIdAnnuncio());
+    // ricerca del commento fatto dall'utente
+    $commentoUtente = Commento::build();
+    $commentoTrovato = false;
+    $i = 0;
+    $l = count($commentiAnnuncio);
+    while ($i < $l && !$commentoTrovato) {
+        if ($commentiAnnuncio[$i]->getIdPrenotazione() !== $id_prenotazione) {
+            $i++;
         } else {
-            header("Location: ./404.php");
+            $commentoTrovato = true;
+            $commentoUtente = $commentiAnnuncio[$i];
         }
-    } catch (Eccezione $ex) {
-        $_SESSION["msg"] = "Non hai il permesso di accedere a questa pagina!";
-        header("Location: ./error_page.php");
     }
     
-} else {
-    header("Location: ./login.php");
+    $content = "";
+    if ($commentoTrovato) {
+        $content = "<ul class=\"riepilogo_annucio\">";
+        $content .= "<li>Titolo: " . $commentoUtente->getTitolo() . "</li>";
+        $content .= "<li>Commento: " . $commentoUtente->getCommento() . "</li>";
+        $content .= "<li>Valutazione: " . $commentoUtente->getValutazione() . "</li>";
+        $content .= "</ul>";
+    } else {
+        $content = "";
+        if ($prenotazioni->getDataFine() < date("Y-m-d")) {
+            $content = file_get_contents("./components/aggiungi_commento_form.html");
+        } else {
+            $content = "<p>Mi dispiace, ma non &egrave; ancora possibile commentare questa prenotazione!</p>";
+        }
+    }
+    $pagina = str_replace("<COMMENTO/>", $content, $pagina);
+    $pagina = str_replace("<IDPRENOTAZIONE/>", $prenotazioni->getIdOccupazione(), $pagina);
+    $pagina = str_replace("<DATAINIZIO/>", $prenotazioni->getDataInizio(), $pagina);
+    $pagina = str_replace("<DATAFINE/>", $prenotazioni->getDataFine(), $pagina);
+    $pagina = str_replace("<MAILPROPRIETARIO/>", $host->getMail(), $pagina);
+    $pagina = str_replace("<NUMOSPITI/>", $prenotazioni->getNumOspiti(), $pagina);
+    $pagina = str_replace("<NOMEANNUNCIO/>", "<a href=\"./dettagli_annuncio.php?id=$id\" title=\"Visualizza altre informazioni dell\'annuncio $titolo\">$titolo</a>", $pagina);
+    $pagina = str_replace("<INDIRIZZO/>", $annuncio->getIndirizzo(), $pagina);
+    $pagina = str_replace("<CITTA/>", $annuncio->getCitta(), $pagina);
+    $pagina = str_replace("<PROPRIETARIO/>", $host->getUserName(), $pagina);
+    $pagina = str_replace("<PREZZO/>", $totale, $pagina);
+    
+    /**
+     * Gestione pubblicazione commento
+     */
+    if(isset($_POST["pubblica_commento"])) {
+        $risultato_validazione = checkValuesForKeysInAssociativeArray($_POST, ["titolo_commento", "testo_commento", "valutazione_commento"]);
+        /**
+         * se $form_non_valido === TRUE ci sono valori di $_POST non settati
+         */
+        $form_non_valido = in_array(FALSE, $risultato_validazione);
+    
+        /**
+         * Valori per la funzione addUserNotificationToPage
+         */
+        $messageToUser = "";
+        $divId = "info_box";
+        $divClasses = "aligned_with_form messaggio_errore";
+        $inParagraph = TRUE; // dice alla funzione addUserNotificationToPage che il contenuto non andrà dentro un paragraph
+    
+        if($form_non_valido) {
+            $messageToUser = formValidationErrorList("<p>C'&egrave; stato un errore nella compilazione del modulo.</p>", $risultato_validazione);
+            $inParagraph = FALSE;
+        } else {
+            try {
+                $codice_inserimento = $frent->insertCommento(
+                    $id_prenotazione,
+                    $_POST["titolo_commento"],
+                    $_POST["testo_commento"],
+                    intval($_POST["valutazione_commento"])
+                );
+                
+                if($codice_inserimento === -1 || $codice_inserimento === -4) { // in caso di prenotazione inesistente (-1) e se il commento non è stato inserito (-4)
+                    $messageToUser = htmlentities("C'è stato un errore nel processo di inserimento del commento.");
+                } elseif($codice_inserimento === -2) { // in caso di prenotazione già commentata
+                    $messageToUser = htmlentities("La prenotazione risulta già commentata.");
+                } elseif($codice_inserimento === -3) { // in caso l'host stia cercando di commentare una prenotazione ad un suo annucio
+                    $messageToUser = htmlentities("Non puoi commentare un tuo annuncio.");
+                } else { // ID del commento appena inserito se l'inserimento è andato a buon fine
+                    // placeholder sostituiti con stringa vuota in quanto avvenuta registrazione
+                    placeholder_replacement_with_empty($pagina);
+                    $messageToUser = htmlentities("Il commento è stato pubblicato.");
+                    $messageToUser .= "<a href=\"dettagli_annuncio.php?id=" . $prenotazioni->getIdAnnuncio() . "\" title=\"Vai a vedere il tuo commento all'interno della sezione commenti dell'annuncio\">Vai a vedere il tuo commento</a>.";
+                    $divClasses = "aligned_with_form messaggio_ok";
+                }
+            } catch(Eccezione $exc) {
+                $messageToUser = htmlentities("C'è stato un errore nel processo di inserimento del commento. Errore riscontrato: ") . $exc->getMessage();
+            }
+        }
+        /// se è stata invocata placeholder_replacement_with_empty, la funzione non avrà side-effect
+        placeholder_replacement_with_content($_POST, $pagina); // aggiornata per riferimento
+        $pagina = addUserNotificationToPage($pagina, $messageToUser, $divId, $divClasses, $inParagraph); // sostituisce <INFO_BOX/>
+    } else {
+        $pagina = str_replace("<INFO_BOX/>", "", $pagina);
+        placeholder_replacement_with_empty($pagina); // aggiornata per riferimento
+    }
+    
+
+    echo $pagina;
+} catch (Eccezione $ex) {
+    echo "Eccezione: " . $exc->getMessage();
 }
